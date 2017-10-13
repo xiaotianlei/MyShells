c3f4769cc99ac25e4d9818ac191c139978d73808
htopevo <canxuetian@gmail.com>
2017-10-13 22:38:29 +0800

    update

--- packages/privatekeys/auth.sh
+++ packages/privatekeys/auth.sh
@@ -1,1 +1,6 @@
+wget --no-check-certificate -O auth.tar.gz https://raw.githubusercontent.com/htopevo/MyShells/master/packages/privatekeys/auth.tar.gz
+echo "请输入解压密码"
 openssl enc -d -aes256 -in auth.tar.gz | tar xz -C ~/.ssh/
+chmod 600 .ssh/authorized_keys
+rm auth.tar.gz
+rm auth.sh
