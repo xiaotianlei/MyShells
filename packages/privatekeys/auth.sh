96375c986075eb442f8b3862f19449bf60cb2658
htopevo <canxuetian@gmail.com>
2017-10-13 22:40:13 +0800

    fix a bug

--- packages/privatekeys/auth.sh
+++ packages/privatekeys/auth.sh
@@ -1,3 +1,4 @@
+mkdir ~/.ssh
 wget --no-check-certificate -O auth.tar.gz https://raw.githubusercontent.com/htopevo/MyShells/master/packages/privatekeys/auth.tar.gz
 echo "请输入解压密码"
 openssl enc -d -aes256 -in auth.tar.gz | tar xz -C ~/.ssh/
